1d9e1951b96e7ff7898df71a8ba9801198912eaa
ewudes <ewudes@gmail.com>
2022-09-07 17:21:58 +0300

    fix: home component

--- src/pages/home/home.tml.ts
+++ src/pages/home/home.tml.ts
@@ -1,96 +1,22 @@
 import Handlebars from "handlebars";
 
 const home = Handlebars.compile(
-  `<main class="container">
-    <h1 class="visually-hidden">Барбершоп «Бородинский»</h1>
-
-    <section class="features">
-      <h2 class="visually-hidden">Преимущества</h2>
-      <ul class="features-list">
-        <li class="feature-item">
-          <h3>Быстро</h3>
-          <p>Мы делаем свою работу быстро! Два часа пролетят незаметно и вы — счастливый обладатель стильной стрижки-минутки!</p>
-        </li>
-        <li class="feature-item">
-          <h3>Круто</h3>
-          <p>Забудьте, как вы стриглись раньше. Мы сделаем из вас звезду футбола или кино! Во всяком случае внешне.</p>
-        </li>
-        <li class="feature-item">
-          <h3>Дорого</h3>
-          <p>Наши мастера — профессионалы своего дела и не могут стоить дешево. К тому же, разве цена не дает определенный статус?</p>
-        </li>
-      </ul>
-    </section>
-
-    <div class="index-columns">
-      <section class="news">
-        <h2>Новости</h2>
-        <ul class="news-list">
-          <li class="news-item">
-            <p>Нам наконец завезли Ягермайстер! Теперь вы можете пропустить стаканчик во время стрижки</p>
-            <time datetime="2016-01-11">11 января</time>
-          </li>
-          <li class="news-item">
-            <p>В нашей команде пополнение, Борис «Бритва» Стригунец, обладатель множества титулов и наград пополнил наши стройные ряды</p>
-            <time datetime="2016-01-18">18 января</time>
-          </li>
-        </ul>
-        <a class="button" href="news.html">Все новости</a>
-      </section>
-
-      <section class="gallery">
-        <h2>Фотогалерея</h2>
-        <figure class="gallery-content">
-          <a href="#"><img src="img/studio.jpg" width="286" height="164" alt="Интерьер"></a>
-        </figure>
-        <button class="button gallery-button gallery-button-back" type="button">Назад</button>
-        <button class="button gallery-button gallery-button-next" type="button">Вперед</button>
-      </section>
-    </div>
-
-    <div class="index-columns">
-      <section class="contacts">
-        <h2>Контактная информация</h2>
-        <p>
-          Барбершоп «Бородинский»<br>
-          Адрес: г. Санкт-Петербург, Б. Конюшенная, д. 19/8<br>
-          Телефон: +7 (812) 666-02-66
-        </p>
-        <p>
-          Время работы:<br>
-          пн—пт: с 10:00 до 22:00<br>
-          сб—вс: с 10:00 до 19:00
-        </p>
-        <a class="button" href="map.html">Как проехать</a>
-        <a class="button" href="contacts.html">Обратная связь</a>
-      </section>
-
-      <section class="appointment">
-        <h2>Записаться</h2>
-        <p class="appointment-info">Укажите желаемую дату и время и мы свяжемся с вами для подтверждения брони</p>
-        <form class="appointment-form" action="https://echo.htmlacademy.ru" method="post">
-          <p class="appointment-item">
-            <label for="appointment-date">Дата</label>
-            <input id="appointment-date" type="date" name="date" value="2017-10-08">
-          </p>
-          <p class="appointment-item">
-            <label for="appointment-time">Время</label>
-            <input id="appointment-time" type="time" name="time" value="10:00">
-          </p>
-          <p class="appointment-item">
-            <label for="appointment-name">Ваше имя</label>
-            <input id="appointment-name" type="text" name="name" value="" placeholder="Борода">
-          </p>
-          <p class="appointment-item">
-            <label for="appointment-phone">Телефон</label>
-            <input id="appointment-phone" type="tel" name="phone" value="" placeholder="+7 123 456-78-90">
-          </p>
-          <button class="button" type="submit">Отправить</button>
-        </form>
-      </section>
-    </div>
-
-  </main>`
+  `<div class="wrapper">
+    {{{header}}}
+    <main class="container">
+      <h1 class="visually-hidden">Барбершоп «Бородинский»</h1>
+      {{{features}}}
+      <div class="index-columns">
+        {{{news}}}
+        {{{gallery}}}
+      </div>
+      <div class="index-columns">
+        {{{contacts}}}
+        {{{appointment}}}
+      </div>
+    </main>
+    {{{footer}}}
+  <div>`
 );
 
 export default home;
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import Block from "../../utils/block";
 import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import Features from "../../components/features/features";
+import News from "../../components/news/news";
+import Gallery from "../../components/gallery/gallery";
+import Contacts from "../../components/contacts/contacts";
+import Appointment from "../../components/appointment/appointment";
 
 import home from "./home.tml";
 import "./home.scss";
@@ -7,8 +13,14 @@ import "./home.scss";
 class Home extends Block {
   constructor(props: Record<string, any> = {}) {
     const header = new Header();
+    const footer = new Footer();
+    const features = new Features();
+    const news = new News();
+    const gallery = new Gallery();
+    const contacts = new Contacts();
+    const appointment = new Appointment();
 
-    super("div", { header, ...props });
+    super("div", { header, features, news, gallery, contacts, appointment, footer, ...props });
   }
 
   render() {
