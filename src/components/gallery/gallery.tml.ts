2391e5f79807af538c79f78c2e3557bfb7274dff
ewudes <ewudes@gmail.com>
2022-09-07 16:01:02 +0300

    add: gallery component

---
+++ src/components/gallery/gallery.tml.ts
@@ -0,0 +1,12 @@
+import Handlebars from "handlebars";
+
+const gallery = Handlebars.compile(
+  `<section class="gallery">
+    <h2 class="gallery__title">Фотогалерея</h2>
+    <figure class="gallery__content"></figure>
+    <button class="button gallery-button gallery-button-back" type="button">Назад</button>
+    <button class="button gallery-button gallery-button-next" type="button">Вперед</button>
+  </section>`
+);
+
+export default gallery;
---
+++ src/components/gallery/gallery.ts
@@ -0,0 +1,19 @@
+import Block from "../../utils/block";
+
+import gallery from "./gallery.tml";
+import './gallery.scss'
+
+interface IGallery {
+}
+
+class Gallery extends Block {
+  constructor(props?: IGallery) {
+    super("div", props);
+  }
+
+  render() {
+    return this.setTemplate(gallery, this.props);
+  }
+}
+
+export default Gallery;
