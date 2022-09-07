eeb1577c915efb40730b4614ddee398ba9907a3b
ewudes <ewudes@gmail.com>
2022-09-07 17:21:29 +0300

    fix: footer component

--- src/components/footer/footer.tml.ts
+++ src/components/footer/footer.tml.ts
@@ -1,24 +1,26 @@
 import Handlebars from "handlebars";
 
 const footer = Handlebars.compile(
-  `<footer class="main-footer">
+  `<footer class="footer">
     <div class="container">
-      <p class="footer-contacts">
+      <p class="footer__contacts">
         Барбершоп «Бородинский»<br>
         Адрес: г. Санкт-Петербург, Б. Конюшенная, д. 19/8<br>
         <a href="map.html">Как нас найти?</a><br>
         Телефон: +7 (812) 666-02-66
       </p>
-      <div class="footer-social">
+      <div class="footer__social">
         <b>Давайте дружить!</b>
         <ul>
-          <li><a class="social-button" href="#">Вконтакте</a></li>
-          <li><a class="social-button" href="#">Фейсбук</a></li>
-          <li><a class="social-button" href="#">Инстаграм</a></li>
+          <li><a class="footer__social-button" href="#">Вконтакте</a></li>
+          <li><a class="footer__social-button" href="#">Фейсбук</a></li>
+          <li><a class="footer__social-button" href="#">Инстаграм</a></li>
         </ul>
       </div>
-      <p class="footer-copyright">
-        <b>Разработано:</b> <a class="button" href="https://htmlacademy.ru">HTML Academy</a>
+      <p class="footer__copyright">
+        <strong>Не является копированием проекта HTML Academy</strong>
+        <strong>Ознакомиться с кодом можно тут:</strong>
+        <a class="button__copyright" href="https://github.com/ewudes/borodinski">GitHub</a>
       </p>
     </div>
   </footer>`
