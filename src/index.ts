657c3e4cac490be5e759fcc15170cdcb497336c7
ewudes <ewudes@gmail.com>
2022-09-09 13:45:23 +0300

    fix: catalog page

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-
 import render from "./utils/render";
 
 import Home from './pages/home/home';
+import Catalog from "./pages/catalog/catalog";
 import './index.scss';
 
 const currentPath: string = window.location.pathname;
@@ -9,6 +9,9 @@ const currentPath: string = window.location.pathname;
 switch(currentPath) {
   case '/':
     render("#app", new Home());
+    break;
+  case '/catalog':
+    render("#app", new Catalog());
     break;
   default:
     render("#app", new Home());
