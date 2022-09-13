1d00b3276d3257dba6a659cb14e0e9cdd4900f1f
ewudes <ewudes@gmail.com>
2022-09-13 13:16:51 +0300

    fix: breadcrumbs style

--- src/components/breadcrumbs/breadcrumbs.tml.ts
+++ src/components/breadcrumbs/breadcrumbs.tml.ts
@@ -2,11 +2,14 @@ import Handlebars from "handlebars";
 
 const breadcrumbs = Handlebars.compile(
   `<ul class="breadcrumbs">
-    <li>
+    <li class="breadcrumbs__item">
       <a href="index.html">Главная</a>
     </li>
-    <li>
+    <li class="breadcrumbs__item">
       <a href="catalog.html">Магазин</a>
+    </li>
+    <li class="breadcrumbs__item">
+      <a href="catalog.html">Средства для ухода</a>
     </li>
   </ul>`
 );
