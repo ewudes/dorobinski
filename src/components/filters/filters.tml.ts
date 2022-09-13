2baa04f8b4e51f38565f588f5c828b79a8cd159f
ewudes <ewudes@gmail.com>
2022-09-13 13:16:36 +0300

    fix: filters style

--- src/components/filters/filters.tml.ts
+++ src/components/filters/filters.tml.ts
@@ -4,49 +4,53 @@ const breadcrumbs = Handlebars.compile(
   `<section class="filters">
     <h2 class="visually-hidden">Фильтр товаров</h2>
     <form class="filter" method="get" action="https://echo.htmlacademy.ru">
-      <fieldset>
+      <fieldset class="filters__group">
         <legend>Производители:</legend>
-        <ul>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-checkbox" type="checkbox" name="baxter-of-california" id="filter-baxter-of-california" checked>
-            <label for="filter-baxter-of-california">Baxter of California</label>
+        <ul class="filters__option-list">
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-checkbox" type="checkbox" name="baxter-of-california" id="filter-baxter-of-california" checked>
+            <label class="filters__input-label" for="filter-baxter-of-california">Baxter of California</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-checkbox" type="checkbox" name="mr-natty" id="filter-mr-natty">
-            <label for="filter-mr-natty">Mr Natty</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-checkbox" type="checkbox" name="mr-natty" id="filter-mr-natty">
+            <label class="filters__input-label" for="filter-mr-natty">Mr Natty</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-checkbox" type="checkbox" name="suavecito" id="filter-suavecito" checked>
-            <label for="filter-suavecito">Suavecito</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-checkbox" type="checkbox" name="suavecito" id="filter-suavecito" checked>
+            <label class="filters__input-label" for="filter-suavecito">Suavecito</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-checkbox" type="checkbox" name="malin-goetz" id="filter-malin-goetz">
-            <label for="filter-malin-goetz">Malin+Goetz</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-checkbox" type="checkbox" name="malin-goetz" id="filter-malin-goetz">
+            <label class="filters__input-label" for="filter-malin-goetz">Malin+Goetz</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-checkbox" type="checkbox" name="murrays" id="filter-murrays">
-            <label for="filter-murrays">Murray’s</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-checkbox" type="checkbox" name="murrays" id="filter-murrays">
+            <label class="filters__input-label" for="filter-murrays">Murray’s</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-checkbox" type="checkbox" name="american-crew" id="filter-american-crew" checked>
-            <label for="filter-american-crew">American Crew</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-checkbox" type="checkbox" name="american-crew" id="filter-american-crew" checked>
+            <label class="filters__input-label" for="filter-american-crew">American Crew</label>
           </li>
         </ul>
       </fieldset>
-      <fieldset>
+      <fieldset class="filters__group">
         <legend>Группы товаров:</legend>
-        <ul>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-radio" type="radio" name="product-group" value="shaving" id="filter-shave">
-            <label for="filter-shave">Бритвенные принадлежности</label>
+        <ul class="filters__option-list">
+        <li class="filters__option">
+          <input class="visually-hidden filters__input filters__input-radio" type="radio" name="product-group" value="shaving" id="filter-shave">
+          <label class="filters__input-label" for="filter-shave">Все</label>
+        </li>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-radio" type="radio" name="product-group" value="shaving" id="filter-shave">
+            <label class="filters__input-label" for="filter-shave">Бритвенные принадлежности</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-radio" type="radio" name="product-group" value="hair-care" id="filter-care" checked>
-            <label for="filter-care">Средства для ухода</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-radio" type="radio" name="product-group" value="hair-care" id="filter-care" checked>
+            <label class="filters__input-label" for="filter-care">Средства для ухода</label>
           </li>
-          <li class="filter-option">
-            <input class="visually-hidden filter-input filter-input-radio" type="radio" name="product-group" value="accessories" id="filter-accessories">
-            <label for="filter-accessories">Аксессуары</label>
+          <li class="filters__option">
+            <input class="visually-hidden filters__input filters__input-radio" type="radio" name="product-group" value="accessories" id="filter-accessories">
+            <label class="filters__input-label" for="filter-accessories">Аксессуары</label>
           </li>
         </ul>
       </fieldset>
