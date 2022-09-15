68d15430a68fcf32ec778cd1b3fba985fa54a9f7
ewudes <ewudes@gmail.com>
2022-09-15 15:19:58 +0300

    fix: routing

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import render from "./utils/render";
 import Home from './pages/home/home';
 import Store from "./pages/store/store";
 import Error from "./pages/error/error";
+import News from "./pages/news/news";
+import Price from "./pages/price/price";
+import Contacts from "./pages/contacts/contacts";
 
 import './index.scss';
 
@@ -12,8 +15,17 @@ switch(currentPath) {
   case '/':
     render("#app", new Home());
     break;
+  case '/news':
+    render("#app", new News());
+    break;
   case '/store':
     render("#app", new Store());
+    break;
+  case '/price':
+    render("#app", new Price());
+    break;
+  case '/contacts':
+    render("#app", new Contacts());
     break;
   default:
     render("#app", new Error());
