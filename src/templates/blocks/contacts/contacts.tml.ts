a803f2af11a86215f646c35b23e48327b1047e13
ewudes <ewudes@gmail.com>
2023-04-10 10:58:30 +0300

    fix: contacts section style

--- src/templates/blocks/contacts/contacts.tml.ts
+++ src/templates/blocks/contacts/contacts.tml.ts
@@ -13,8 +13,10 @@ const contacts = Handlebars.compile(
       пн—пт: с 10:00 до 22:00<br>
       сб—вс: с 10:00 до 19:00
     </p>
-    <a class="button" href="map.html">Как проехать</a>
-    <a class="button" href="contacts.html">Обратная связь</a>
+    <div class="contacts__buttons">
+      <a class="button" href="map.html">Как проехать</a>
+      <a class="button" href="contacts.html">Обратная связь</a>
+    </div>
   </section>`
 );
 
