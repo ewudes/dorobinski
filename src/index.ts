270b7e8b5a71563ee1cbdc5b17cb501a2a0f567c
ewudes <ewudes@gmail.com>
2023-11-16 13:31:38 +0300

    add: mocks

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,15 @@ import Contacts from "./templates/pages/contacts/contacts";
 import Item from "./templates/pages/item/item";
 
 import router from "./core/router";
+import { filterStore, filterNews } from "./mocks/filters";
+import { catalog } from "./mocks/catalog";
 
 import "./index.scss";
 
 router
   .use("/", Home)
-  .use("/news", News)
-  .use("/store", Store)
+  .use("/news", News, filterNews)
+  .use("/store", Store, [filterStore, catalog])
   .use("/price", Price)
   .use("/contacts", Contacts)
   .use(`/store/item`, Item)
