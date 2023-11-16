4b581b2e2d86eb93e4a622c38a4937ac15ba3eaf
ewudes <ewudes@gmail.com>
2023-11-16 13:31:01 +0300

    fix: catalog mocks

--- src/templates/blocks/catalog/catalog.ts
+++ src/templates/blocks/catalog/catalog.ts
@@ -6,66 +6,18 @@ import catalog from "./catalog.tml";
 import "./catalog.scss";
 
 interface ICatalog {
+  map: any;
 }
 
 class Catalog extends Block {
-  constructor(props?: ICatalog) {
-    const catalog = [
-      {
-        item: new Item({
-          id: 1,
-          img: "./img/content/product-1.jpg",
-          description: "Набор для путешествий  «Baxter of California»",
-          price: 2990,
-          category: "shaving"
-        }),
-      },
-      {
-        item: new Item({
-          id: 2,
-          img: "./img/content/product-2.jpg",
-          description: "Увлажняющий кондиционер  «Baxter of California»",
-          price: 1550,
-          category: "shaving"
-        }),
-      },
-      {
-        item: new Item({
-          id: 3,
-          img: "./img/content/product-3.jpg",
-          description: "Гель для волос «SUAVECITO»",
-          price: 490,
-          category: "shaving"
-        }),
-      },
-      {
-        item: new Item({
-          id: 4,
-          img: "./img/content/product-4.jpg",
-          description: "Глина для укладки волос  «American crew»",
-          price: 990,
-          category: "shaving"
-        }),
-      },
-      {
-        item: new Item({
-          id: 5,
-          img: "./img/content/product-5.jpg",
-          description: "Гель для волос «AMERICAN CREW»",
-          price: 3790,
-          category: "shaving"
-        }),
-      },
-      {
-        item: new Item({
-          id: 6,
-          img: "./img/content/product-6.jpg",
-          description: "Набор для бритья «Baxter of California»",
-          price: 4830,
-          category: "shaving"
-        })
-      }
-    ]
+  constructor(props: ICatalog) {
+
+    const catalog = props.map((x: any) => ({
+      item: new Item({
+        ...x
+      })
+    }));
+
     const pagination = new Pagination();
 
     super("div", {
