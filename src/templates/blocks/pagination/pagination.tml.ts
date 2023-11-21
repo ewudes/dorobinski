5899431ebf56dc49d286a96a6821f3f54718bbfd
ewudes <ewudes@gmail.com>
2023-11-21 14:23:52 +0300

    fix: pagination template

--- src/templates/blocks/pagination/pagination.tml.ts
+++ src/templates/blocks/pagination/pagination.tml.ts
@@ -5,7 +5,7 @@ const pagination = Handlebars.compile(
     <li class="pagination-prev"><a href="#">&laquo;</a></li>
 
     {{#each pages}}
-      <li class="pagination-page" data-page="{{this}}"><a href="#">{{this}}</a></li>
+    <li class="pagination-page" data-page="{{this}}"><a href="#">{{this}}</a></li>
     {{/each}}
 
     <li class="pagination-next"><a href="#">&raquo;</a></li>
